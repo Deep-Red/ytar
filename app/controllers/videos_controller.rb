f835758bde4d5a24a9d0f81e443c3b7d661d9bf0
Deep-Red <kirbycolebrown@gmail.com>
2018-02-19 08:44:07 -0500

    Retrieving page from example url and parsing out result videos ids.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+  require 'open-uri'
+
   def index
     @search_terms = params[:video][:list].split("\r\n")
   end
@@ -10,6 +12,19 @@ class VideosController < ApplicationController
   def viewer
     @search_terms = params[:video][:list].split("\r\n")
     @offset = params[:video][:offset].to_i || @offset = 0
+    @video_list = []
+    @image_url_list = []
+
+    current_search = @search_terms[@offset]
+    search_results = Nokogiri::HTML(open("http://www.youtube.com/results?search_query=#{current_search}"))
+
+    search_results.xpath("//img").each do |image|
+      @image_url_list << (image.values.select { |v| v[/https:\/\/i.ytimg.com\/vi\/*/]}[0]) unless image.values.select { |v| v[/https:\/\/i.ytimg.com\/vi\/*/]}.empty?
+    end
+
+    @image_url_list.each do |url|
+      @video_list << url[/https:\/\/i.ytimg.com\/vi\/(.*)\//, 1]
+    end
   end
 
   def accept
