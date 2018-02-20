a0ca1a77defd6d4cbc3ce502610418a274525013
Deep-Red <kirbycolebrown@gmail.com>
2018-02-20 14:33:49 -0500

    Work on async calls for loading next video. Having to pass a lot of data back and forth, will probably try using a session or cookie to simplify things abit.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require bootstrap
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
   require 'open-uri'
+  before_action :sanitize_page_params, only: [:viewer, :accept, :reject, :retry]
 
   def index
     @search_terms = params[:video][:list].split("\r\n")
@@ -12,25 +13,67 @@ class VideosController < ApplicationController
   def viewer
     @search_terms = params[:video][:list].split("\r\n")
     @offset = params[:video][:offset].to_i || @offset = 0
+    @attempt_number ||= 0
 
     set_videos_and_urls if (@image_url_list.nil? || @image_url_list.empty?)
 
-    @video = "https://www.youtube.com/embed/#{@video_list[@offset]}?rel=0&autoplay=true"
+    @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   def accept
+    @search_terms = params[:video][:list].split("\r\n")
+    @offset = 1 + params[:video][:offset]
+    @attempt_number = 0
+    set_videos_and_urls
+
+    @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
+
+    respond_to do |format|
+      format.html {redirect_to videos_viewer_path}
+      format.js
+    end
   end
 
   def reject
+    @search_terms = params[:video][:list].split("\r\n")
+    @offset = 1 + params[:video][:offset]
+    @attempt_number = 0
+    set_videos_and_urls
+
+    @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
+
+    respond_to do |format|
+      format.html {redirect_to videos_viewer_path}
+      format.js
+    end
   end
 
   def retry
+    @video_list = params[:video][:list].split("\r\n")
+    @attempt_number = params[:video][:list].to_i
+    @offset = params[:video][:list].to_i
+
+    @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
+
+    respond_to do |format|
+      format.html {redirect_to videos_viewer_path}
+      format.js
+    end
   end
 
   private
   def set_videos_and_urls
     @video_list ||= []
     @image_url_list ||= []
+    @search_terms ||= params[:video][:list]
+    @offset = params[:video][:offset].to_i
+    @attempt_number = params[:video][:attempt_number].to_i
 
     current_search = @search_terms[@offset]
     search_results = Nokogiri::HTML(open("http://www.youtube.com/results?search_query=#{current_search}"))
@@ -45,6 +88,12 @@ class VideosController < ApplicationController
   end
 
   def videos_params
-    params.permit(:list, :offset)
+    params.permit(:list, :offset, :attempt_number)
   end
+
+  def sanitize_page_params
+    params[:offset] = params[:offset].to_i
+    params[:attempt_number] = params[:attempt_number].to_i
+  end
+
 end
