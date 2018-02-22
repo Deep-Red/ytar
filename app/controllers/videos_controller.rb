fd4a5172ab76bd4c96c552b79f92035e79cb9e50
Deep-Red <kirbycolebrown@gmail.com>
2018-02-21 20:56:01 -0500

    videos#accept basic function (modifying session and moving to next search term) and general cleanum and refactoring of videos controller and 'viewer' view.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,7 +9,9 @@ class VideosController < ApplicationController
 
     set_session_videos_and_urls
 
-    @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
+    video_list = session[:video_list]
+    attempt_number = session[:attempt_number]
+    @video = "https://www.youtube.com/embed/#{video_list[attempt_number]}?rel=0&autoplay=true"
 
     respond_to do |format|
       format.html
@@ -18,12 +20,22 @@ class VideosController < ApplicationController
   end
 
   def accept
-    @search_terms = params[:video][:list].split("\r\n")
-    @offset = 1 + params[:video][:offset]
-    @attempt_number = 0
-    set_session_videos_and_urls
+    search_terms = session[:search_terms]
+    video_list = session[:video_list]
+    attempt_number = session[:attempt_number]
+    session[:accepted] << video_list[attempt_number]
 
-    @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
+    session[:attempt_number] = 0
+    attempt_number = 0
+    session[:offset] += 1
+    offset = session[:offset]
+
+    current_search = search_terms[offset]
+
+    video_list = set_video_list(current_search)
+    session[:video_list] = video_list
+
+    @video = "https://www.youtube.com/embed/#{video_list[attempt_number]}?rel=0&autoplay=true"
 
     respond_to do |format|
       format.html {redirect_to videos_viewer_path}
@@ -46,11 +58,10 @@ class VideosController < ApplicationController
   end
 
   def retry
-    @video_list = session[:video_list]
-    @attempt_number = (session[:attempt_number] += 1)
-    @offset = session[:offset]
+    video_list = session[:video_list]
+    attempt_number = (session[:attempt_number] += 1)
 
-    @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
+    @video = "https://www.youtube.com/embed/#{video_list[attempt_number]}?rel=0&autoplay=true"
 
     respond_to do |format|
       format.html {redirect_to videos_viewer_path}
@@ -60,13 +71,22 @@ class VideosController < ApplicationController
 
   private
   def set_session_videos_and_urls
-    @search_terms = params[:video][:list].split("\r\n")
-    @offset = 0
-    @attempt_number = 0
-    image_url_list = []
-    @video_list = []
+    search_terms = params[:video][:list].split("\r\n")
+    offset = 0
+    attempt_number = 0
+    video_list = []
 
-    current_search = @search_terms[@offset]
+    current_search = search_terms[offset]
+
+    video_list = set_video_list(current_search)
+
+    initialize_session_variables(search_terms, video_list, offset, attempt_number)
+
+  end
+
+  def set_video_list(current_search)
+    image_url_list = []
+    video_list = []
     search_results = Nokogiri::HTML(open("http://www.youtube.com/results?search_query=#{current_search}"))
 
     search_results.xpath("//img").each do |image|
@@ -74,14 +94,18 @@ class VideosController < ApplicationController
     end
 
     image_url_list.each do |url|
-      @video_list << url[/https:\/\/i.ytimg.com\/vi\/(.*)\//, 1]
+      video_list << url[/https:\/\/i.ytimg.com\/vi\/(.*)\//, 1]
     end
+    return video_list
+  end
 
-    session[:video_list] = @video_list
-    session[:image_url_list] = @image_url_list
-    session[:search_terms] = @search_terms
-    session[:offset] = @offset
-    session[:attempt_number] = @attempt_number
+  def initialize_session_variables(st, vl, o, an)
+    session[:search_terms] = st
+    session[:video_list] = vl
+    session[:offset] = o
+    session[:attempt_number] = an
+    session[:accepted] = []
+    session[:rejected] = []
   end
 
   def videos_params
