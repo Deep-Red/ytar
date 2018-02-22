256d87330e87a686a3798b91eaf3c4df443c3d57
Deep-Red <kirbycolebrown@gmail.com>
2018-02-21 19:17:33 -0500

    Retry working to grab the next video on the list. Using sessions now instead of trying to pass the ~4 objects back and forth with each request. This may be misguided, I see that sessions seem to be generally discouraged for anything beyond a user or session id, but with such a small amount of data I don't feel justified in creating a database.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,12 @@
 class VideosController < ApplicationController
   require 'open-uri'
-  before_action :sanitize_page_params, only: [:viewer, :accept, :reject, :retry]
 
   def index
     @search_terms = params[:video][:list].split("\r\n")
   end
 
   def show
-    @params = params[:search_terms]
+    @search_terms = session[:search_terms]
   end
 
   def viewer
@@ -15,7 +14,8 @@ class VideosController < ApplicationController
     @offset = params[:video][:offset].to_i || @offset = 0
     @attempt_number ||= 0
 
-    set_videos_and_urls if (@image_url_list.nil? || @image_url_list.empty?)
+    set_session_videos_and_urls if (@image_url_list.nil? || @image_url_list.empty?)
+
 
     @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
 
@@ -30,7 +30,7 @@ class VideosController < ApplicationController
     @search_terms = params[:video][:list].split("\r\n")
     @offset = 1 + params[:video][:offset]
     @attempt_number = 0
-    set_videos_and_urls
+    set_session_videos_and_urls
 
     @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
 
@@ -44,7 +44,7 @@ class VideosController < ApplicationController
     @search_terms = params[:video][:list].split("\r\n")
     @offset = 1 + params[:video][:offset]
     @attempt_number = 0
-    set_videos_and_urls
+    set_session_videos_and_urls
 
     @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
 
@@ -55,9 +55,9 @@ class VideosController < ApplicationController
   end
 
   def retry
-    @video_list = params[:video][:list].split("\r\n")
-    @attempt_number = params[:video][:list].to_i
-    @offset = params[:video][:list].to_i
+    @video_list = session[:video_list]
+    @attempt_number = (session[:attempt_number] += 1)
+    @offset = session[:offset]
 
     @video = "https://www.youtube.com/embed/#{@video_list[@attempt_number]}?rel=0&autoplay=true"
 
@@ -68,32 +68,33 @@ class VideosController < ApplicationController
   end
 
   private
-  def set_videos_and_urls
-    @video_list ||= []
-    @image_url_list ||= []
-    @search_terms ||= params[:video][:list]
-    @offset = params[:video][:offset].to_i
-    @attempt_number = params[:video][:attempt_number].to_i
+  def set_session_videos_and_urls
+    @search_terms = params[:video][:list].split("\r\n")
+    @offset = session[:offset] || 0
+    @attempt_number = session[:attempt_number] || 0
+    image_url_list = []
+    @video_list = []
 
     current_search = @search_terms[@offset]
     search_results = Nokogiri::HTML(open("http://www.youtube.com/results?search_query=#{current_search}"))
 
     search_results.xpath("//img").each do |image|
-      @image_url_list << (image.values.select { |v| v[/https:\/\/i.ytimg.com\/vi\/*/]}[0]) unless image.values.select { |v| v[/https:\/\/i.ytimg.com\/vi\/*/]}.empty?
+      image_url_list << (image.values.select { |v| v[/https:\/\/i.ytimg.com\/vi\/*/]}[0]) unless image.values.select { |v| v[/https:\/\/i.ytimg.com\/vi\/*/]}.empty?
     end
 
-    @image_url_list.each do |url|
+    image_url_list.each do |url|
       @video_list << url[/https:\/\/i.ytimg.com\/vi\/(.*)\//, 1]
     end
+
+    session[:video_list] = @video_list
+    session[:image_url_list] = @image_url_list
+    session[:search_terms] = @search_terms
+    session[:offset] = @offset
+    session[:attempt_number] = @attempt_number
   end
 
   def videos_params
     params.permit(:list, :offset, :attempt_number)
-  end
-
-  def sanitize_page_params
-    params[:offset] = params[:offset].to_i
-    params[:attempt_number] = params[:attempt_number].to_i
   end
 
 end
