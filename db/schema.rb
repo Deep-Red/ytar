55bad641bdfe5270c913672ca27a58bb5967bd76
Deep-Red <kirbycolebrown@gmail.com>
2018-05-09 07:00:44 -0400

    Switch to postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180225082719) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
     t.text "data"
