5e921bba23e7dce83d1a915422d8f1485b0f0db1
Deep-Red <kirbycolebrown@gmail.com>
2018-05-10 04:20:11 -0400

    Cleanup.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,7 +4,7 @@ class VideosController < ApplicationController
   require 'zip'
 
   def index
-    @search_terms = videos_params["list"].split("\r\n")
+    @search_terms = videos_params[:list].split("\r\n")
   end
 
   def viewer
@@ -180,7 +180,7 @@ class VideosController < ApplicationController
 
   private
   def set_session_videos_and_urls
-    search_terms = videos_params["list"].split("\r\n")
+    search_terms = videos_params[:list].split("\r\n")
     search_terms.map! { |st| CGI.escape(st) }
     offset = 0
     attempt_number = 0
